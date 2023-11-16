8057183d723348ea8df90ea11ed2f4bae60bf44b
Vishnu-D-2002 <vishnulosangels@gmail.com>
2023-11-16 08:47:21 +0530

    changes

--- index.js
+++ index.js
@@ -1,38 +1,57 @@
-// Import required modules
 const express = require('express');
-const fs = require('fs');
+const fs = require('fs').promises;
 
-// Create an Express application
 const app = express();
-
-// Generate a timestamp to be used in the filename
-const currentTime = new Date().toISOString().replace(/:/g, '_');
-
-// Specify the port on which the server will listen
 const PORT = 3001;
 
-// Configure Express to parse incoming JSON data
 app.use(express.json());
 
-// Define a route to handle GET requests to the root endpoint ('/')
-app.get('/', (req, res) => {
-  // Read the content of a file with the generated filename
-  fs.readFile(`./${currentTime}.txt`, 'utf-8', (err, data) => {
-    if (err) {
-      throw err;
-    } else {
-      console.log('All files are read successfully');
-      res.send(data);
+// Handle GET requests to the root endpoint ('/')
+app.get('/', async (req, res) => {
+  try {
+    // Read the list of files in the current directory
+    const files = await fs.readdir('./', { withFileTypes: true });
+
+    // Read the contents of each file (not directories) and filter out non-text files
+    const fileContents = [];
+    for (const file of files) {
+      // Check if it's a text file and not the 'doc.txt' file
+      if (file.isFile() && file.name.endsWith('.txt') && file.name !== 'doc.txt') {
+        // Read the content of the file
+        const data = await fs.readFile(`./${file.name}`, 'utf-8');
+        // Push the filename and content to the array
+        fileContents.push({ filename: file.name, content: data });
+      }
     }
-  });
+
+    console.log('All text files are read successfully');
+    // Send the array of file contents as the response
+    res.json(fileContents);
+  } catch (err) {
+    // Handle errors
+    console.error(err);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
-// Write the current timestamp into a new file
-fs.writeFile(`./${currentTime}.txt`, currentTime, { flag: 'w+' }, (err) => {
-  if (err) {
-    console.log(err);
-  } else {
+// Handle POST requests to the root endpoint ('/')
+app.post('/', async (req, res) => {
+  try {
+    // Create a timestamp for the filename
+    const currentTime = new Date().toISOString().replace(/:/g, '_');
+    // Create the filename with the timestamp
+    const filename = `./${currentTime}.txt`;
+
+    // Write the current timestamp into a new file
+    await fs.writeFile(filename, currentTime, { flag: 'w+' });
     console.log('File added successfully');
+
+    // Send the filename and content in the response
+    res.status(201).json({ filename, content: currentTime });
+  } catch (err) {
+    // Handle errors
+    console.error(err);
+    res.status(500).send('Internal Server Error');
   }
 });
 
