beeff91cf09dd360837f9ec8215ef7f08f2d28e2
Vishnu-D-2002 <vishnulosangels@gmail.com>
2023-11-20 16:36:40 +0530

    full content

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const fs = require('fs');
-const {MongoDB_URI,PORT} = require('./utils/config');
+const {MONGODB_URI,PORT} = require('./utils/config');
 const cors = require('cors');
 const app = express();
 app.use(cors());
@@ -8,9 +8,12 @@ app.use(express.json());
 
 const mongoose = require('mongoose');
 
-mongoose.connect(MongoDB_URI)
+mongoose.connect(MONGODB_URI)
   .then(() => {
     console.log('connected to MongoDB...');
+    app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+  });
   })
   .catch((err) => {
     console.error(err);
@@ -20,7 +23,7 @@ const noteSchema = new mongoose.Schema({
   id: Number,
   content: String,
   important: Boolean
-});
+},{versionKey:false});
 
 const Note = mongoose.model("Note", noteSchema, "notes");
 
@@ -31,6 +34,66 @@ app.get('/', (req, res) => {
     })
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
+app.get('/:id', (req, res) => {
+  const ID = req.params.id;
+  Note.findOne({ id: ID })
+    .then((note) => {
+      res.status(200).json(note);
+    })
+    .catch(err => {
+      res.status(404).json({ message: "Note not found" });
+  })
+});
+
+app.post('/', (req, res) => {
+  const note = new Note(req.body);
+  note.save()
+    .then(note => {
+      res.status(201).json("Note created successfully",note);
+    })
+    .catch(err => {
+      res.status(404).json({ message: "Error in finding id" })
+    })
+});
+
+app.put('/:id', async (req, res) => {
+  try {
+    const note = await Note.findOneAndUpdate({ id: req.params.id }, req.body, { new: true });
+    if(!note)
+      return  res.status(404).json({ message: "ID not found" });
+    res.status(200).json({message:"Note changed successfully", note});
+  } catch (err) {
+    res.status(500).json({ message: "server error"});
+  }
+});
+
+app.patch('/:id', async (req, res) => {
+  try {
+    // Exclude the _id field from the update
+    const { _id, ...updateData } = req.body;
+    const note = await Note.findOneAndUpdate({ id: req.params.id },updateData,{ new: true });
+    if (!note) {
+      return res.status(404).json({ message: "Id not found" });
+    } else {
+      return res.status(200).json({ message: "Note patched successfully", note });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal error" });
+  }
+});
+
+app.delete('/:id', async (req, res) => {
+  try {
+    const note = await Note.findOneAndDelete({ id: req.params.id });
+    if (!note) {
+      return res.status(404).json({ message: "Id not found" });
+    }
+    else {
+      return res.status(200).json({ message: "Note deleted successfully" });
+    }
+  } catch (error) {
+    console.error('error', error);
+    res.status(500).json({ message: "Internal error" });
+  }
 });
